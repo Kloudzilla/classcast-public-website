1e7c3f92690b34e3aa3753b91b3a873436af0746
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-07-16 14:59:34 +1000

    Classcast Public Site Blog and Thumbnail changes

--- wordpress/wp-content/themes/classcast-2014/index.php
+++ wordpress/wp-content/themes/classcast-2014/index.php
@@ -9,36 +9,24 @@
  
     <div class="cc-section">
       <div class="w-container cc-content-container cc-blog-content">
-        <div class="w-clearfix cc-blog-post">
-          <img class="w-hidden-tiny cc-blog-post-author-img" src="<?php echo get_template_directory_uri(); ?>/img/image-placeholder.svg" alt="image-placeholder.svg">
-          <div class="cc-blog-post-content">
-            <div class="cc-blog-post-headline"><a class="cc-blog-post-headline-link" href="blog-post.html">Case Study: How KettleWorkX Uses Classcast To Engage Instructors</a>
-            </div>
-            <div class="cc-blog-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique ipsum non commodo laoreet. Vivamus ut imperdiet orci. Maecenas dapibus nisi tellus, ac facilisis metus tristique et. Morbi adipiscing accumsan lectus non rutrum. Duis
-              facilisis id enim eget iaculis. Nulla tellus arcu, ultricies ac dictum at, commodo in nibh. Morbi eu lorem imperdiet, posuere velit semper, convallis dolor.</div>
-            <div class="cc-blog-post-author-text">By Firstname Last – May 26th, 2014</div>
-          </div>
-        </div>
-        <div class="w-clearfix cc-blog-post">
-          <img class="w-hidden-tiny cc-blog-post-author-img" src="<?php echo get_template_directory_uri(); ?>/img/image-placeholder.svg" alt="image-placeholder.svg">
-          <div class="cc-blog-post-content">
-            <div class="cc-blog-post-headline"><a href="blog-post.html" class="cc-blog-post-headline-link">Case Study: How KettleWorkX Uses Classcast To Engage Instructors</a>
-            </div>
-            <div class="cc-blog-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique ipsum non commodo laoreet. Vivamus ut imperdiet orci. Maecenas dapibus nisi tellus, ac facilisis metus tristique et. Morbi adipiscing accumsan lectus non rutrum. Duis
-              facilisis id enim eget iaculis. Nulla tellus arcu, ultricies ac dictum at, commodo in nibh. Morbi eu lorem imperdiet, posuere velit semper, convallis dolor.</div>
-            <div class="cc-blog-post-author-text">By Firstname Last – May 26th, 2014</div>
-          </div>
-        </div>
-        <div class="w-clearfix cc-blog-post">
-          <img class="w-hidden-tiny cc-blog-post-author-img" src="<?php echo get_template_directory_uri(); ?>/img/image-placeholder.svg" alt="image-placeholder.svg">
-          <div class="cc-blog-post-content last">
-            <div class="cc-blog-post-headline"><a class="cc-blog-post-headline-link" href="blog-post.html">Case Study: How KettleWorkX Uses Classcast To Engage Instructors</a>
-            </div>
-            <div class="cc-blog-post-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tristique ipsum non commodo laoreet. Vivamus ut imperdiet orci. Maecenas dapibus nisi tellus, ac facilisis metus tristique et. Morbi adipiscing accumsan lectus non rutrum. Duis
-              facilisis id enim eget iaculis. Nulla tellus arcu, ultricies ac dictum at, commodo in nibh. Morbi eu lorem imperdiet, posuere velit semper, convallis dolor.</div>
-            <div class="cc-blog-post-author-text">By Firstname Last – May 26th, 2014</div>
-          </div>
-        </div>
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>   
+              <div class="w-clearfix cc-blog-post">
+                <?php if ( has_post_thumbnail() ) { ?>
+                  <div class="w-hidden-tiny cc-blog-post-author-img"><?php the_post_thumbnail('thumbnail');?></div>
+                <?php 
+                  }
+                ?>
+                <div class="cc-blog-post-content">
+                  <div class="cc-blog-post-headline"><a class="cc-blog-post-headline-link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                  </div>
+                  <div class="cc-blog-post-text"><?php the_content(); ?></div>
+                  <div class="cc-blog-post-author-text">By <?php the_author() ?> – <?php the_time('F jS, Y') ?></div>
+                </div>
+              </div>
+          <?php endwhile;
+        endif;?> 
+             
       </div>
     </div>
                 
