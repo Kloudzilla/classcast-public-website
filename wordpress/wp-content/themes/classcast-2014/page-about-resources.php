13f1ba67d37fed71d3fdfab2f7cb16a1f02648af
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-07-23 13:19:45 +1000

    Classcast Public Site Resources Revamp

--- wordpress/wp-content/themes/classcast-2014/page-about-resources.php
+++ wordpress/wp-content/themes/classcast-2014/page-about-resources.php
@@ -9,8 +9,7 @@
 <?php
 	get_header();
 ?>
- 
-      <div class="w-hidden-small w-hidden-tiny cc-section cc-about-tagline">
+  <div class="cc-section cc-about-tagline">
       <div class="w-container cc-content-container cc-about-tagline-content">
         <h4 class="cc-about-resources-tagline">Your best source of knowledge and education to get your mobile content monetized</h4>
       </div>
@@ -18,101 +17,80 @@
     <div class="cc-section">
       <div class="w-container cc-content-container cc-about-team">
         <h3 class="cc-about-resources-headline">EBOOKS</h3>
-        <div class="w-row cc-ebooks-featured-row">
-          <div class="w-col w-col-4 cc-ebooks-featured-col">
-            <div class="cc-ebook-featured"></div>
-            <div class="cc-ebook-featured-title">CLASSCAST&nbsp;V2.0: FITNESS&nbsp;&amp;&nbsp;TRAINING</div>
-            <a class="w-inline-block cc-button-s blue-keyline" href="#">
-              <div>Download</div>
+        <div class="w-row cc-ebook-row">
+          <div class="w-col w-col-3 cc-ebook-col">
+            <div class="cc-ebook-cover"></div>
+            <h5 class="cc-ebook-title">Heading</h5>
+            <a class="w-inline-block cc-button-s keyline" href="#">
+              <div>Download.</div>
             </a>
           </div>
-          <div class="w-col w-col-4 cc-ebooks-featured-col">
-            <div class="cc-ebook-featured"></div>
-            <div class="cc-ebook-featured-title">WHAT&nbsp;IS&nbsp;MOBILE?</div>
-            <a class="w-inline-block cc-button-s blue-keyline" href="#">
-              <div>Download</div>
+          <div class="w-col w-col-3 cc-ebook-col">
+            <div class="cc-ebook-cover"></div>
+            <h5 class="cc-ebook-title">Heading</h5>
+            <a class="w-inline-block cc-button-s keyline" href="#">
+              <div>Download.</div>
             </a>
           </div>
-          <div class="w-col w-col-4 cc-ebooks-featured-col">
-            <div class="cc-ebook-featured"></div>
-            <div class="cc-ebook-featured-title">WHY&nbsp;CLASSCAST?</div>
-            <a class="w-inline-block cc-button-s blue-keyline" href="#">
-              <div>Download</div>
+          <div class="w-col w-col-3 cc-ebook-col">
+            <div class="cc-ebook-cover"></div>
+            <h5 class="cc-ebook-title">Heading</h5>
+            <a class="w-inline-block cc-button-s keyline" href="#">
+              <div>Download.</div>
             </a>
           </div>
-        </div>
-        <div class="w-row cc-ebooks-row">
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
-          </div>
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
-          </div>
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
-          </div>
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
-          </div>
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
-          </div>
-          <div class="w-col w-col-2 w-col-medium-4 w-col-small-6 w-col-tiny-6">
-            <div class="cc-ebook"></div>
-            <div class="cc-ebook-title">EBOOK&nbsp;TITLE</div><a class="cc-ebook-download" href="#">Download</a>
+          <div class="w-col w-col-3 cc-ebook-col">
+            <div class="cc-ebook-cover"></div>
+            <h5 class="cc-ebook-title">Heading</h5>
+            <a class="w-inline-block cc-button-s keyline" href="#">
+              <div>Download.</div>
+            </a>
           </div>
         </div>
         <h3 class="cc-about-resources-headline">WEBINARS</h3>
-        <div class="cc-video-featured"></div>
-        <div class="cc-video-featured-title">CLASSCAST&nbsp;PODCAST&nbsp;EPISODE 234: WHAT’s FITNESS&nbsp;TO&nbsp;YOU?</div>
-        <div class="w-row cc-videos-row">
-          <div class="w-col w-col-3 w-col-small-6">
-            <div class="cc-video"></div>
-            <div class="cc-video-title">VIDEO&nbsp;TITLE</div>
+        <div class="w-row cc-video-row">
+          <div class="w-col w-col-3">
+            <div class="cc-video-cover"></div>
+            <h5 class="cc-video-title">Heading</h5>
           </div>
-          <div class="w-col w-col-3 w-col-small-6">
-            <div class="cc-video"></div>
-            <div class="cc-video-title">VIDEO&nbsp;TITLE</div>
+          <div class="w-col w-col-3">
+            <div class="cc-video-cover"></div>
+            <h5 class="cc-video-title">Heading</h5>
           </div>
-          <div class="w-col w-col-3 w-col-small-6">
-            <div class="cc-video"></div>
-            <div class="cc-video-title">VIDEO&nbsp;TITLE</div>
+          <div class="w-col w-col-3">
+            <div class="cc-video-cover"></div>
+            <h5 class="cc-video-title">Heading</h5>
           </div>
-          <div class="w-col w-col-3 w-col-small-6">
-            <div class="cc-video"></div>
-            <div class="cc-video-title">VIDEO&nbsp;TITLE</div>
+          <div class="w-col w-col-3">
+            <div class="cc-video-cover"></div>
+            <h5 class="cc-video-title">Heading</h5>
           </div>
         </div>
         <h3 class="cc-about-resources-headline">TUTORIALS</h3>
         <div class="w-row">
           <div class="w-col w-col-3">
-            <div class="cc-video"></div>
+            <div class="cc-video-cover"></div>
           </div>
           <div class="w-col w-col-9">
-            <div class="cc-tutorial-heading">What is Classcast?</div>
+            <h5 class="cc-tutorial-title">Heading</h5>
             <div class="cc-tutorial-subtext">A general introduction to the platforms features and capabilities</div>
           </div>
         </div>
         <div class="w-row">
           <div class="w-col w-col-3">
-            <div class="cc-video"></div>
+            <div class="cc-video-cover"></div>
           </div>
           <div class="w-col w-col-9">
-            <div class="cc-tutorial-heading">What ‘mobile’ means for your business</div>
+            <h5 class="cc-tutorial-title">Heading</h5>
             <div class="cc-tutorial-subtext">A look into the complexities of a constantly moving industry</div>
           </div>
         </div>
         <div class="w-row">
           <div class="w-col w-col-3">
-            <div class="cc-video"></div>
+            <div class="cc-video-cover"></div>
           </div>
           <div class="w-col w-col-9">
-            <div class="cc-tutorial-heading">Managing thousands of users</div>
+            <h5 class="cc-tutorial-title">Heading</h5>
             <div class="cc-tutorial-subtext">Discover how Classcast can make managing thousands of users simple</div>
           </div>
         </div>
