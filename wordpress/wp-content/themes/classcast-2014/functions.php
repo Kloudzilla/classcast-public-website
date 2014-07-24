e05aae944d58c5ff66f45756d3b329fe565a1250
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-07-24 16:28:35 +1000

    Classcast Public Site Resources Page Attachment Functionality

--- wordpress/wp-content/themes/classcast-2014/functions.php
+++ wordpress/wp-content/themes/classcast-2014/functions.php
@@ -24,68 +24,69 @@ function new_excerpt_more( $more ) {
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
 
-add_action('init', 'cptui_register_my_cpt_job');
-function cptui_register_my_cpt_job() {
-register_post_type('job', array(
-'label' => 'Jobs',
-'description' => '',
-'public' => true,
-'show_ui' => true,
-'show_in_menu' => true,
-'capability_type' => 'post',
-'map_meta_cap' => true,
-'hierarchical' => false,
-'rewrite' => array('slug' => 'job', 'with_front' => true),
-'query_var' => true,
-'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
-'labels' => array (
-  'name' => 'Jobs',
-  'singular_name' => 'Job',
-  'menu_name' => 'Jobs',
-  'add_new' => 'Add Job',
-  'add_new_item' => 'Add New Job',
-  'edit' => 'Edit',
-  'edit_item' => 'Edit Job',
-  'new_item' => 'New Job',
-  'view' => 'View Job',
-  'view_item' => 'View Job',
-  'search_items' => 'Search Jobs',
-  'not_found' => 'No Jobs Found',
-  'not_found_in_trash' => 'No Jobs Found in Trash',
-  'parent' => 'Parent Job',
-)
-) ); }
-add_action('init', 'cptui_register_my_cpt_press');
-function cptui_register_my_cpt_press() {
-register_post_type('press', array(
-'label' => 'Press',
-'description' => '',
-'public' => true,
-'show_ui' => true,
-'show_in_menu' => true,
-'capability_type' => 'post',
-'map_meta_cap' => true,
-'hierarchical' => false,
-'rewrite' => array('slug' => 'press', 'with_front' => true),
-'query_var' => true,
-'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
-'labels' => array (
-  'name' => 'Press',
-  'singular_name' => 'Press',
-  'menu_name' => 'Press',
-  'add_new' => 'Add Press',
-  'add_new_item' => 'Add New Press',
-  'edit' => 'Edit',
-  'edit_item' => 'Edit Press',
-  'new_item' => 'New Press',
-  'view' => 'View Press',
-  'view_item' => 'View Press',
-  'search_items' => 'Search Press',
-  'not_found' => 'No Press Found',
-  'not_found_in_trash' => 'No Press Found in Trash',
-  'parent' => 'Parent Press',
-)
-) ); }
+// add_action('init', 'cptui_register_my_cpt_job');
+// function cptui_register_my_cpt_job() {
+// register_post_type('job', array(
+// 'label' => 'Jobs',
+// 'description' => '',
+// 'public' => true,
+// 'show_ui' => true,
+// 'show_in_menu' => true,
+// 'capability_type' => 'post',
+// 'map_meta_cap' => true,
+// 'hierarchical' => false,
+// 'rewrite' => array('slug' => 'job', 'with_front' => true),
+// 'query_var' => true,
+// 'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
+// 'labels' => array (
+//   'name' => 'Jobs',
+//   'singular_name' => 'Job',
+//   'menu_name' => 'Jobs',
+//   'add_new' => 'Add Job',
+//   'add_new_item' => 'Add New Job',
+//   'edit' => 'Edit',
+//   'edit_item' => 'Edit Job',
+//   'new_item' => 'New Job',
+//   'view' => 'View Job',
+//   'view_item' => 'View Job',
+//   'search_items' => 'Search Jobs',
+//   'not_found' => 'No Jobs Found',
+//   'not_found_in_trash' => 'No Jobs Found in Trash',
+//   'parent' => 'Parent Job',
+// )
+// ) ); }
+// add_action('init', 'cptui_register_my_cpt_press');
+// function cptui_register_my_cpt_press() {
+// register_post_type('press', array(
+// 'label' => 'Press',
+// 'description' => '',
+// 'public' => true,
+// 'show_ui' => true,
+// 'show_in_menu' => true,
+// 'capability_type' => 'post',
+// 'map_meta_cap' => true,
+// 'hierarchical' => false,
+// 'rewrite' => array('slug' => 'press', 'with_front' => true),
+// 'query_var' => true,
+// 'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes','post-formats'),
+// 'labels' => array (
+//   'name' => 'Press',
+//   'singular_name' => 'Press',
+//   'menu_name' => 'Press',
+//   'add_new' => 'Add Press',
+//   'add_new_item' => 'Add New Press',
+//   'edit' => 'Edit',
+//   'edit_item' => 'Edit Press',
+//   'new_item' => 'New Press',
+//   'view' => 'View Press',
+//   'view_item' => 'View Press',
+//   'search_items' => 'Search Press',
+//   'not_found' => 'No Press Found',
+//   'not_found_in_trash' => 'No Press Found in Trash',
+//   'parent' => 'Parent Press',
+// )
+// ) ); }
+
  
 function get_custom_post_type_template($single_template) {
      global $post;
--- wordpress/wp-content/themes/classcast-2014/page-about-resources.php
+++ wordpress/wp-content/themes/classcast-2014/page-about-resources.php
@@ -10,92 +10,66 @@
 	get_header();
 ?>
   <div class="cc-section cc-about-tagline">
-      <div class="w-container cc-content-container cc-about-tagline-content">
-        <h4 class="cc-about-resources-tagline">Your best source of knowledge and education to get your mobile content monetized</h4>
-      </div>
+    <div class="w-container cc-content-container cc-about-tagline-content">
+      <h4 class="cc-about-resources-tagline">Your best source of knowledge and education to get your mobile content monetized</h4>
     </div>
-    <div class="cc-section">
-      <div class="w-container cc-content-container cc-about-team">
-        <h3 class="cc-about-resources-headline">EBOOKS</h3>
-        <div class="w-row cc-ebook-row">
-          <div class="w-col w-col-3 cc-ebook-col">
-            <div class="cc-ebook-cover"></div>
-            <h5 class="cc-ebook-title">Heading</h5>
-            <a class="w-inline-block cc-button-s keyline" href="#">
-              <div>Download.</div>
-            </a>
-          </div>
-          <div class="w-col w-col-3 cc-ebook-col">
-            <div class="cc-ebook-cover"></div>
-            <h5 class="cc-ebook-title">Heading</h5>
-            <a class="w-inline-block cc-button-s keyline" href="#">
-              <div>Download.</div>
-            </a>
-          </div>
-          <div class="w-col w-col-3 cc-ebook-col">
-            <div class="cc-ebook-cover"></div>
-            <h5 class="cc-ebook-title">Heading</h5>
-            <a class="w-inline-block cc-button-s keyline" href="#">
-              <div>Download.</div>
-            </a>
-          </div>
-          <div class="w-col w-col-3 cc-ebook-col">
-            <div class="cc-ebook-cover"></div>
-            <h5 class="cc-ebook-title">Heading</h5>
-            <a class="w-inline-block cc-button-s keyline" href="#">
-              <div>Download.</div>
-            </a>
-          </div>
+  </div>
+  <div class="cc-section">
+    <div class="w-container cc-content-container cc-about-team">
+      <h3 class="cc-about-resources-headline">EBOOKS</h3>
+      <div class="w-row cc-ebook-row">
+        <?php $loop = new WP_Query( array( 'post_type' => 'ebook' ) ); ?>
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>     
+        <div class="w-col w-col-3 cc-ebook-col">
+          <div class="cc-ebook-cover"></div>
+          <h5 class="cc-ebook-title"><?php the_title(); ?></h5>
+          <?php $ebook = get_post_meta($post->ID, 'ebook_release', true); ?>
+          <?php if ($ebook): ?>
+          <a href="<?php echo wp_get_attachment_url($ebook); ?>" class="w-inline-block cc-button-s keyline" >Download</a>
+          <?php endif; ?>          
         </div>
-        <h3 class="cc-about-resources-headline">WEBINARS</h3>
-        <div class="w-row cc-video-row">
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-            <h5 class="cc-video-title">Heading</h5>
-          </div>
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-            <h5 class="cc-video-title">Heading</h5>
-          </div>
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-            <h5 class="cc-video-title">Heading</h5>
-          </div>
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-            <h5 class="cc-video-title">Heading</h5>
-          </div>
-        </div>
-        <h3 class="cc-about-resources-headline">TUTORIALS</h3>
-        <div class="w-row">
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-          </div>
-          <div class="w-col w-col-9">
-            <h5 class="cc-tutorial-title">Heading</h5>
-            <div class="cc-tutorial-subtext">A general introduction to the platforms features and capabilities</div>
-          </div>
-        </div>
-        <div class="w-row">
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-          </div>
-          <div class="w-col w-col-9">
-            <h5 class="cc-tutorial-title">Heading</h5>
-            <div class="cc-tutorial-subtext">A look into the complexities of a constantly moving industry</div>
-          </div>
-        </div>
-        <div class="w-row">
-          <div class="w-col w-col-3">
-            <div class="cc-video-cover"></div>
-          </div>
-          <div class="w-col w-col-9">
-            <h5 class="cc-tutorial-title">Heading</h5>
-            <div class="cc-tutorial-subtext">Discover how Classcast can make managing thousands of users simple</div>
-          </div>
-        </div>
+        <?php endwhile; ?>
       </div>
+
+      <h3 class="cc-about-resources-headline">VIDEOS</h3>
+      <div class="w-row cc-video-row">
+        <?php $loop = new WP_Query( array( 'post_type' => 'video' ) ); ?>
+        <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>   
+        <div class="w-col w-col-3 cc-video-col">
+          <?php $video = get_post_meta($post->ID, 'video_release', true); ?>
+          <?php if ($video): ?>
+          <a href="<?php echo wp_get_attachment_url($video); ?>">
+            <div class="cc-video-cover"></div>
+            <h5 class="cc-video-title"><?php the_title(); ?></h5>
+          </a>
+          <?php endif; ?> 
+        </div>
+        <?php endwhile; ?>
+      </div>
+
+      <iframe src="//player.vimeo.com/video/46748490" width="500" height="313" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+      <h3 class="cc-about-resources-headline">TUTORIALS</h3>
+      <?php $loop = new WP_Query( array( 'post_type' => 'tutorial', 'order' => 'ASC') ); ?>
+      <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>   
+      <div class="w-row">
+        <?php $tutorial = get_post_meta($post->ID, 'tutorial_release', true); ?>
+        <?php if ($tutorial): ?>
+        <a href="<?php echo wp_get_attachment_url($tutorial); ?>">
+          <div class="w-col w-col-3">
+            <div class="cc-video-cover"></div>
+          </div>
+          <div class="w-col w-col-9">
+            <h5 class="cc-tutorial-title"><?php the_title(); ?></h5>
+            <div class="cc-tutorial-subtext"><?php the_excerpt(); ?></div>
+          </div>  
+        </a> 
+        <?php endif; ?>   
+      </div>
+      <?php endwhile; ?>
+
     </div>
+  </div>
                 
 
 <?php
