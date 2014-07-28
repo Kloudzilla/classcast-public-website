7b2e648871423d5a0ca78d75773046145b497b20
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-07-28 16:26:18 +1000

    Classcast Public Site Vimeo Fix

--- wordpress/wp-content/themes/classcast-2014/page-about-resources.php
+++ wordpress/wp-content/themes/classcast-2014/page-about-resources.php
@@ -36,9 +36,18 @@
         <?php $loop = new WP_Query( array( 'post_type' => 'video' ) ); ?>
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>   
         <div class="w-col w-col-3 cc-video-col">
-          <?php $video = get_post_meta($post->ID, 'video_release', true); ?>
-          <?php if ($video): ?>
-          <a href="<?php echo wp_get_attachment_url($video); ?>" class="w-inline-block cc-video-link modal-video-link">
+          <div class="w-clearfix modal-video-background">
+            <a class="w-inline-block close-modal-video" href="#">
+              <div>CLOSE</div>
+            </a>
+            <div class="w-container">
+              <div class="modal-video-window">
+                <?php $videoID = get_post_meta($post->ID, 'tutorial_release', true);?>
+                <iframe src="<?php echo 'http://player.vimeo.com/video/'. $videoID;?>" width="600" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+              </div>
+            </div>
+          </div>
+          <a href="#" class="w-inline-block cc-video-link modal-video-link">
             <div class="cc-video-cover"></div>
             <h5 class="cc-video-title"><?php the_title(); ?></h5>
           </a>
@@ -46,8 +55,6 @@
         </div>
         <?php endwhile; ?>
       </div>
-
-      <iframe src="//player.vimeo.com/video/46748490" width="500" height="313" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 
       <h3 class="cc-about-resources-headline">TUTORIALS</h3>
       <?php $loop = new WP_Query( array( 'post_type' => 'tutorial', 'order' => 'ASC') ); ?>
@@ -59,17 +66,11 @@
       </a>
       <div class="w-container">
         <div class="modal-video-window">
-          <?php
-          // Get the video URL and put it in the $video variable
-          $videoID = get_post_meta($post->ID, 'tutorial_release', true);
-          // Echo the embed code via oEmbed
-          echo wp_oembed_get( 'http://vimeo.com/' . $videoID );
-          ?>
+          <?php $videoID = get_post_meta($post->ID, 'tutorial_release', true);?>
+          <iframe src="<?php echo 'http://player.vimeo.com/video/'. $videoID;?>" width="600" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
         </div>
       </div>
     </div>
-
-
       <div class="w-row">
         <!--<?php $tutorial = get_post_meta($post->ID, 'tutorial_release', true); ?>
         <?php if ($tutorial): ?>-->
