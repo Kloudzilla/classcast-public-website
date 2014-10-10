37590c78d0dd1a59a27622d18c209fb3572c2c88
Dallas Clark <dallas@dallasjclark.com>
2014-10-10 11:33:33 +1000

    Created backup of the production site before upgrading Wordpress

---
+++ backup/html/wordpress/wp-content/themes/classcast-2014/page-pricing.php
@@ -0,0 +1,279 @@
+<?php
+  /*
+  Template Name: Pricing Page
+  */
+  global $template_section;
+  $template_section = 'pricing';
+?>
+
+<?php
+  get_header();
+?>
+
+      <div class="cc-section">
+      <div class="w-container cc-content-container cc-pricing-content">
+        <div class="cc-request-demo-title">Connect with one of our team for a free demo. No obligation, no credit card required.</div>
+        <div class="w-tabs" data-duration-in="300" data-duration-out="100">
+          <div class="w-tab-menu cc-pricing-tab-menu">
+            <a class="w-tab-link w-inline-block cc-pricing-tab monthly" data-w-tab="Tab 1">
+              <div>Billed Monthly</div>
+            </a>
+            <a class="w-tab-link w--current w-inline-block cc-pricing-tab annual" data-w-tab="Tab 2">
+              <div>Billed Annually</div>
+            </a>
+          </div>
+          <div class="w-tab-content">
+            <div class="w-tab-pane" data-w-tab="Tab 1">
+              <div class="cc-pricing-grid-wrap">
+                <div class="w-row cc-pricing-grid-row">
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">STARTER</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">299</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>100</strong> users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">-</li>
+                        <li class="cc-pricing-grid-col-list-item">-</li>
+                        <li class="cc-pricing-grid-col-list-item">-&nbsp;
+                          <br>&nbsp;</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col">
+                    <a class="w-inline-block cc-pricing-package-link featured modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package featured">
+                        <div class="cc-pricing-package-title featured">GROW</div>
+                        <div class="cc-pricing-package-subline">Most Popular</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap featured">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">499</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list featured">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>200</strong>&nbsp;users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">-&nbsp;
+                          <br>&nbsp;</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col second-row">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">MAX</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">899</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>4<strong xmlns="http://www.w3.org/1999/xhtml">00</strong></strong>users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">Go to market and growth support</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col second-row">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">ENTERPRISE</div>
+                        <div class="cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-price-enterprise">Need more than
+                            <br>2000 users?</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">Over <strong>2,000</strong> users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">Go to market and growth support</li>
+                      </ul>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-tab-pane w--tab-active" data-w-tab="Tab 2">
+              <div class="cc-pricing-grid-wrap">
+                <div class="w-row cc-pricing-grid-row">
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">STARTER</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">269</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>100</strong> users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">-</li>
+                        <li class="cc-pricing-grid-col-list-item">-</li>
+                        <li class="cc-pricing-grid-col-list-item">-&nbsp;
+                          <br>&nbsp;</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col">
+                    <a class="w-inline-block cc-pricing-package-link featured modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package featured">
+                        <div class="cc-pricing-package-title featured">GROW</div>
+                        <div class="cc-pricing-package-subline">Most Popular</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap featured">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">449</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list featured">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>200</strong>&nbsp;users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">-&nbsp;
+                          <br>&nbsp;</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col second-row">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">MAX</div>
+                        <div class="w-clearfix cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-currency">$</div>
+                          <div class="cc-pricing-package-price">809</div>
+                          <div class="cc-pricing-package-month">/mo</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">up to <strong>4<strong xmlns="http://www.w3.org/1999/xhtml">00</strong></strong>users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">Go to market and growth support</li>
+                      </ul>
+                    </a>
+                  </div>
+                  <div class="w-col w-col-3 w-col-small-6 cc-pricing-grid-col second-row">
+                    <a class="w-inline-block cc-pricing-package-link modal-link" href="#">
+                      <div class="cc-pricing-grid-col-package">
+                        <div class="cc-pricing-package-title">ENTERPRISE</div>
+                        <div class="cc-pricing-package-price-wrap">
+                          <div class="cc-pricing-package-price-enterprise">Need more than
+                            <br>400 users?</div>
+                        </div>
+                      </div>
+                      <ul class="w-list-unstyled cc-pricing-grid-col-list">
+                        <li class="cc-pricing-grid-col-list-item">Over <strong>400</strong> users</li>
+                        <li class="cc-pricing-grid-col-list-item">Branded iPhone app</li>
+                        <li class="cc-pricing-grid-col-list-item">Subscription billing</li>
+                        <li class="cc-pricing-grid-col-list-item">Training bookings</li>
+                        <li class="cc-pricing-grid-col-list-item">Music licensing access*</li>
+                        <li class="cc-pricing-grid-col-list-item">Multi currency</li>
+                        <li class="cc-pricing-grid-col-list-item">Go to market and growth support</li>
+                      </ul>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cc-request-demo-title">Free Demo. No obligation, no credit card required.</div>
+        <a class="w-inline-block cc-button-h center request-demo" href="/sign-up">
+          <div>REQUEST&nbsp;A&nbsp;DEMO</div>
+        </a>
+        <h4 class="cc-pricing-faq-title">Answering your questions</h4>
+        <div class="cc-pricing-faq-wrap">
+          <div class="w-row cc-pricing-faq-row">
+            <div class="w-col w-col-6 cc-pricing-faq-col">
+              <ul class="w-list-unstyled cc-pricing-faq-col-list">
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">How long are the contracts?</div>
+                  <div class="cc-pricing-faq-text">You can choose the billing period when signing up (1mo, 3mo or 12mo). You can start and stop your service any time.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">What if I go over my chosen plan?</div>
+                  <div class="cc-pricing-faq-text">Classcast limits your usage and users by the plans and notifies the account holder when the limit is approaching. We don’t automatically change or upgrade your plan.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">Do you offer a discount for annual plans?</div>
+                  <div class="cc-pricing-faq-text">Yes of course. We’re just in the process of making this automated, for now please let your sales associate know! Simple as that.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">What about Music Licensing?</div>
+                  <div class="cc-pricing-faq-text">Classcast offers a comprehensive music licensing plan that covers all your bases. Contact us to find out more.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">What credit cards do you accept?</div>
+                  <div class="cc-pricing-faq-text">We accept all major credit cards (VISA, MasterCard, Discover, American Express). For annual payments we can also invoice you for payment.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">Need more help before signing up?</div>
+                  <div class="cc-pricing-faq-text">If you have questions about Classcast, our pricing, features or the sign up process, please feel free to contact us at hello@classcast.co – we’re always here to help you!</div>
+                </li>
+              </ul>
+            </div>
+            <div class="w-col w-col-6 cc-pricing-faq-col">
+              <ul class="w-list-unstyled cc-pricing-faq-col-list">
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">Can I change plans?</div>
+                  <div class="cc-pricing-faq-text">Yes all non-enterprise plans can be changed any time. Enterprise plans are managed by case-by-case basis.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">What kind of security do you have</div>
+                  <div class="cc-pricing-faq-text">Classcast uses secure 256-bit SSL encryption for all accounts and all data is stored securely. For more on privacy, see our privacy policies and terms of service.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">Do you offer non-profit discounts</div>
+                  <div class="cc-pricing-faq-text">While we don’t have specific discounts for non-profits or educational organizations, we do offer a discount when you sign up for an annual account. Please email us at hello@classcast.co as we might be able to help.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">Hosting and storage included?</div>
+                  <div class="cc-pricing-faq-text">All videos, music, text... well anything is hosted for as long as you remain with us.</div>
+                </li>
+                <li class="cc-pricing-faq-col-list-item">
+                  <div class="cc-pricing-faq-headline">What happens when I want to cancel an account?</div>
+                  <div class="cc-pricing-faq-text">This would be a very sad moment, but we understand that these things happen. From your point of view, nothing. Once you cancel your account you’ll simply never be billed again. To cancel your account please just send as an email and
+                    we’ll process your request.</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php
+  get_footer();
+?>
