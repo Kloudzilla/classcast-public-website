24892a0f6db50f7c6ec2143fd0b4b96ed022be36
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-10-29 15:15:12 +1000

    Onboarding Form Fix - Added default JS to stop redirect

--- wordpress/wp-content/themes/classcast-2014/page-form-signup.php
+++ wordpress/wp-content/themes/classcast-2014/page-form-signup.php
@@ -43,6 +43,7 @@
                         <div id="mc_embed_signup">
                         <form action="//classcast.us2.list-manage.com/subscribe/post?u=1c01ae6d41b7efa731691c63a&amp;id=168b1a7f71" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                             <div id="mc_embed_signup_scroll">
+                            <div class="indicates-required">All fields are required</div>
                        <div class="mc-field-group">
                           <label for="mce-FULLNAME">Full Name
                         </label>
@@ -93,17 +94,16 @@
 
                           </select>
                         </div>
-                          <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_1c01ae6d41b7efa731691c63a_168b1a7f71" tabindex="-1" value=""></div>
-                            <div class="clear"><input type="submit" value="Get Started with your demo!" name="subscribe" id="mc-embedded-subscribe" class="cc-button-s blue"></div>
+                        <div id="mce-responses" class="clear">
+                          <div class="response" id="mce-error-response" style="display:none"></div>
+                          <div class="response" id="mce-success-response" style="display:none"></div>
+                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                          <div style="position: absolute; left: -5000px;"><input type="text" name="b_1c01ae6d41b7efa731691c63a_168b1a7f71" tabindex="-1" value=""></div>
+                          <div class="clear"><input type="submit" value="GET STARTED" name="subscribe" class="cc-button-l get-started"></div>
                           </div>
-                        </form>
-                        </div>
-
-                        <!--End mc_embed_signup-->
+                      </form>
+                      </div>
+                      <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[3]='FULLNAME';ftypes[3]='text';fnames[1]='CNAME';ftypes[1]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[2]='PNUMBER';ftypes[2]='text';fnames[5]='INDUSTRY';ftypes[5]='dropdown';fnames[6]='PACKAGE';ftypes[6]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
             <div>Once you’ve submitted we’ll be in contact shortly to schedule your demo or arrange to help you get started.
           </div>
         </div>
