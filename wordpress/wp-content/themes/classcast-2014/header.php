4ea889ca407044f420daa19091eaf9a5ce1c74c3
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-11-24 14:44:59 +1000

    Facebook Conversion Pixel Code

--- wordpress/wp-content/themes/classcast-2014/header.php
+++ wordpress/wp-content/themes/classcast-2014/header.php
@@ -247,6 +247,22 @@
     <?php
       if($template_section == 'home') {
     ?>
+      <!-- Facebook Conversion Code for Clicks to web -->
+        <script>(function() {
+          var _fbq = window._fbq || (window._fbq = []);
+          if (!_fbq.loaded) {
+            var fbds = document.createElement('script');
+            fbds.async = true;
+            fbds.src = '//connect.facebook.net/en_US/fbds.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(fbds, s);
+            _fbq.loaded = true;
+          }
+        })();
+        window._fbq = window._fbq || [];
+        window._fbq.push(['track', '6017829039898', {'value':'0.00','currency':'USD'}]);
+        </script>
+        <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6017829039898&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
 
         <div class="cc-section">
           <div class="w-slider cc-home-hero-slider" data-animation="slide" data-duration="800" data-infinite="1">
