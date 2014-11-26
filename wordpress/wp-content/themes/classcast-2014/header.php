f0b139d67f397ea1797d25946140eabc51180c20
SamJamStew <simplecue@Simplecues-MacBook-Pro.local>
2014-11-26 15:20:48 +1000

    Facebook Pixel Retargeting Code

--- wordpress/wp-content/themes/classcast-2014/header.php
+++ wordpress/wp-content/themes/classcast-2014/header.php
@@ -264,7 +264,24 @@
         window._fbq.push(['track', '6017829039898', {'value':'0.00','currency':'USD'}]);
         </script>
         <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6017829039898&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" /></noscript>
-
+        
+        <script>(function() {
+          var _fbq = window._fbq || (window._fbq = []);
+          if (!_fbq.loaded) {
+            var fbds = document.createElement('script');
+            fbds.async = true;
+            fbds.src = '//connect.facebook.net/en_US/fbds.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(fbds, s);
+            _fbq.loaded = true;
+          }
+          _fbq.push(['addPixelId', '623174104459253']);
+        })();
+        window._fbq = window._fbq || [];
+        window._fbq.push(['track', 'PixelInitialized', {}]);
+        </script>
+        <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=623174104459253&amp;ev=PixelInitialized" /></noscript>
+        
         <div class="cc-section">
           <div class="w-slider cc-home-hero-slider" data-animation="slide" data-duration="800" data-infinite="1">
             <div class="w-slider-mask">
