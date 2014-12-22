9333b4793d6c1b87d3529e5e1d31cf9c1d9fe17c
Aidan Green <aidan@simplecue.com>
2014-12-22 16:45:05 +1100

    Fix missing jQuery in footer

--- wordpress/wp-content/themes/classcast-2014/footer.php
+++ wordpress/wp-content/themes/classcast-2014/footer.php
@@ -70,6 +70,8 @@
     </div>
   </div>
   
+  
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/webflow.js"></script>
   <!--[if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
    <!--MODAL-->
